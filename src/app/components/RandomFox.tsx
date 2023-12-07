b42acef54c3b6d8cf1caec2b9755c25222e48c52
Willd233 <emiliod255@gmail.com>
2023-12-07 13:04:31 -0400

    update incorporation function random

--- src/app/components/RandomFox.tsx
+++ src/app/components/RandomFox.tsx
@@ -1,9 +1,11 @@
-export const RandomFox = (): JSX.Element => {
-    const randomImageNumber =
-        Math.floor(Math.random() * 123) + 1;
-
-    const image =
-        `https://randomfox.ca/images/${randomImageNumber}.jpg`;
-    return <img width={300} height="auto"
-        src={image} alt="Random Fox" />;
-};
+export const RandomFox = ({ imageURL }: { imageURL: string }) => {
+    return (
+      <img
+        width={300}
+        height="auto"
+        src={imageURL}
+        alt="Random Fox"
+      />
+    );
+  };
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { RandomFox } from "./components/RandomFox";
 
 export default function Home() {
+  const randomImageNumber = Math.floor(Math.random() * 123) + 1;
+  const imageURL = `https://randomfox.ca/images/${randomImageNumber}.jpg`;
+
   return (
- <main>
-  <h1>holaaa</h1>
-  <RandomFox/>
- </main>
-  )
+    <main>
+      <h1>holaaa</h1>
+      <RandomFox imageURL={imageURL} />
+    </main>
+  );
 }
