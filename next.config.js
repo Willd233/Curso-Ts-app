96fb142b2a2c4b753d9d05005c020e2ff23df5ff
Willd233 <emiliod255@gmail.com>
2023-12-13 11:01:52 -0400

    sds

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   output: 'export',
  
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // Optional: Change linkds `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
