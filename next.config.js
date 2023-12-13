9f370238a8b475086b38ad0bcfe77b03a9fdc3d3
Willd233 <emiliod255@gmail.com>
2023-12-13 01:01:36 -0400

    remuve

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    typescript:{
-        ignoreBuildErrors: true
-    }
+    "output": "export"
+
 }
 
 module.exports = nextConfig
